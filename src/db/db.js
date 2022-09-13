8d8185d4610243f70208aca7245b7f6dcd6734a2
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-09-13 16:53:50 -0300

    configuração de ambiente

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+export default async function mongo(){
+    let connect;
+
+    try {
+        connect = await mongoClient.db('mywallet');
+    return connect;
+    } catch (error) {
+        console.error(error);
+        return error
+    }
+}
