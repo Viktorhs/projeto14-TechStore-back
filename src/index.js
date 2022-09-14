dedd22a2d7473d69a7b605553fa24344208882f9
Franciane <franciane.penina@gmail.com>
2022-09-14 17:37:08 -0300

    feat: rota de cadastro implementada

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from "express";
 import cors from "cors";
+import authRouter from "./routes/authRouters.js"
 
 dotenv.config();
 
@@ -8,5 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use(authRouter);
 
 app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`))
