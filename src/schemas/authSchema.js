77b81a1d1803271dae97f3922106bcb8629b31b6
Franciane <franciane.penina@gmail.com>
2022-09-15 04:51:27 -0300

    esqueci de mudar de branch

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -4,6 +4,8 @@ const signUpSchema = joi.object({
     name: joi.string().min(2).required(),
     email: joi.string().email().required(),
     password: joi.string().min(4).required()
-})
+});
+
+
 
 export { signUpSchema };
