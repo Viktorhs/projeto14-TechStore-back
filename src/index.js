7c44077d9dfabaa4ef81a0fab9a9415f9ce97a4f
Franciane <franciane.penina@gmail.com>
2022-09-18 15:02:45 -0300

    feat: rota de checkout

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/authRouters.js"
 import productsRouter from "./routes/productsRouters.js"
-
+import checkoutRouter from './routes/checkoutRouter.js';
 
 dotenv.config();
 
@@ -14,6 +14,6 @@ app.use(express.json());
 
 app.use(authRouter);
 app.use(productsRouter);
-
+app.use(checkoutRouter);
 
 app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`))
